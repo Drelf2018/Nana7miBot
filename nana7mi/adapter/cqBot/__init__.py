6c5985226ac47614a12c8e67c5d89007addb05fa
Drelf2018 <drelf2018@outlook.com>
2022-07-22 03:56:00 +0800

    这 bug 都给我找出来了

--- nana7mi/adapter/cqBot/__init__.py
+++ nana7mi/adapter/cqBot/__init__.py
@@ -1,4 +1,4 @@
 _private = '{\"action\": \"send_private_msg\", \"params\": {\"user_id\": %d, \"message\": \"%s\"}}'
-_group = '{\"action\": \"send_group_msg\", \"params\": {\"group_id\": %d, \"message\": "\%s\"}}'
+_group = '{\"action\": \"send_group_msg\", \"params\": {\"group_id\": %d, \"message\": \"%s\"}}'
 from .cqbot import cqBot
 from .event import Mate, Message, Event, get_event_from_msg
