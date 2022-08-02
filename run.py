7156b0e8ec7dd06564fd3c6e0c62cdd7f70fc8ae
Drelf2018 <drelf2018@outlook.com>
2022-08-02 17:52:42 +0800

    feat: 最新最in的链式调用

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
-﻿import asyncio
-from nana7mi import cqBot, get_bot
+﻿from nana7mi import cqBot, get_bot
 
-bot = get_bot(cqbot=cqBot())
-# bot.load_buildin_plugins()
-bot.load_plugins('./plugins')
-asyncio.run(bot.run())
+get_bot(cqbot=cqBot()).load_buildin_plugins().load_plugins('./plugins').run()
