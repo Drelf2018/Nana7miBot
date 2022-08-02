0a69aeb6bec68e14bd66fb6cd9fbd276ed7c485a
Drelf2018 <drelf2018@outlook.com>
2022-08-03 04:31:19 +0800

    feat: 新增频道事件，并修改程序结构

--- nana7mi/adapters/cqBot/__init__.py
+++ nana7mi/adapters/cqBot/__init__.py
@@ -1,4 +1,74 @@
 _private = '{\"action\": \"send_private_msg\", \"params\": {\"user_id\": %d, \"message\": \"%s\"}}'
 _group = '{\"action\": \"send_group_msg\", \"params\": {\"group_id\": %d, \"message\": \"%s\"}}'
+_guild = '{\"action\": \"send_guild_channel_msg\", \"params\": {\"guild_id\": %s, \"channel_id\": %s, \"message\": \"%s\"}}'
+
+
+async def void_async_func():
+    return ''
+
+def limit(
+    white_channel = list(),
+    banned_channel = list(),
+    white_group = list(),
+    banned_group = list(),
+    white_user = list(),
+    banned_user = list(),
+    at_me=False,
+    channel_both_user=False,
+    group_both_user=False
+):
+    def check_event(white: list, banned: list, eid: int) -> bool:
+        if white:
+            if eid in white:
+                return True
+        elif banned:
+            if eid not in banned:
+                return True
+        else:
+            return True
+        return False
+
+    check_user = lambda user_id: check_event(white_user, banned_user, user_id)
+    check_group = lambda group_id: check_event(white_group, banned_group, group_id)
+    check_channel = lambda channel_id: check_event(white_channel, banned_channel, channel_id)
+
+    def check(func):
+        @wraps(func)
+        def wrapped_function(event: Message):
+            match event.message_type:
+                case MessageType.Private:
+                    return func(event) if check_user(event.user_id) else void_async_func()
+                case MessageType.Group:
+                    if at_me and not event.at_me:
+                        return void_async_func()
+                    if group_both_user:
+                        if not check_user(event.user_id):
+                            return void_async_func()
+                    return func(event) if check_group(event.group_id) else void_async_func()
+                case MessageType.Guild:
+                    if at_me and not event.at_me:
+                        return void_async_func()
+                    if channel_both_user:
+                        if not check_user(event.user_id):
+                            return void_async_func()
+                    return func(event) if check_channel(event.channel_id) else void_async_func()
+        return wrapped_function
+    return check
+
+def on_command(command=None):
+    def check(func):
+        @wraps(func)
+        def wrapped_function(event: Message):
+            if command:
+                if (pos := len(command)) > len(str(event)):
+                    return void_async_func()
+                elif str(event)[:pos] != command:
+                    return void_async_func()
+            event.real_content = str(event).replace(command, '')
+            return func(event)
+        return wrapped_function
+    return check
+
+from functools import wraps
 from .cqbot import cqBot
-from .event import Mate, Message, Event, get_event_from_msg
+from .event import Event, Mate, Message, MessageType, get_event_from_msg
--- nana7mi/adapters/cqBot/event.py
+++ nana7mi/adapters/cqBot/event.py
@@ -1,162 +1,82 @@
 import re
-from functools import wraps
 from json import loads
+from typing import List
 
-from . import _group, _private
+from . import _group, _private, _guild
 
-    
-class Event():
-    def __init__(self):
-        self.isResponse = False
-        self.isAccept = False
 
-    def accept(self):
-        self.isAccept = True
+class MessageType:
+    Private = 'private'
+    Group   = 'group'
+    Guild   = 'guild'
 
-    def ignore(self):
-        self.isAccept = False
-
-    def reply(self, text):
-        if isinstance(text, str):
-            if self.isGroup:
-                return _group % (int(self.group_id), str(text))
-            else:
-                return _private % (int(self.user_id), str(text))
-        elif isinstance(text, (list, tuple)):
-            if self.isGroup:
-                return [_group % (int(self.group_id), str(t)) for t in text]
-            else:
-                return [_private % (int(self.user_id), str(t)) for t in text]
-
+class Event:
+    def reply(self, msgs: str | list | tuple) -> str | List[str]:
+        '根据消息类型以及输入文字返回可发送至 go-cqhttp 的对应 json 语句'
+        if isinstance(msgs, str):
+            match self.message_type:
+                case MessageType.Private:
+                    return _private % (int(self.user_id), msgs)
+                case MessageType.Group:
+                    return _group % (int(self.group_id), msgs)
+                case MessageType.Guild:
+                    return _guild % (self.guild_id, self.channel_id, msgs)
+        elif isinstance(msgs, (list, tuple)):
+            match self.message_type:
+                case MessageType.Private:
+                    return [_private % (int(self.user_id), str(msg)) for msg in msgs]
+                case MessageType.Group:
+                    return [_group % (int(self.group_id), str(msg)) for msg in msgs]
+                case MessageType.Guild:
+                    return [_guild % (self.guild_id, self.channel_id, msg) for msg in msgs]
 
 class Mate(Event):
     def __init__(self, js):
-        super().__init__()
-        self.event_type = js.get('meta_event_type')
-        self.self_id = js.get('self_id')
         self.time = js.get('time')
-        if self.event_type == 'lifecycle':
-            self.sub_type = js.get('sub_type')
-        elif self.event_type == 'heartbeat':
-            self.interval = js.get('interval')
-
-
-async def void_func():
-    return ''
-
+        self.event_type = js.get('meta_event_type')
+        match self.event_type:
+            case 'lifecycle':
+                self.sub_type = js.get('sub_type')
+            case 'heartbeat':
+                self.interval = js.get('interval')
 
 class Message(Event):
-    def __init__(self, js):
-        super().__init__()
-        self.args = None
-        self.sender = js.get('sender')
+    def __init__(self, js: dict):
+        # 消息设置
+        self.message_type = js.get('message_type')
+        # 通用参数
         self.time = js.get('time')
         self.self_id = js.get('self_id')
         self.message_id = js.get('message_id')
+        self.message = js.get('message')
+        self.raw_message = js.get('raw_message')
         self.user_id = js.get('user_id')
-        self.message = js.get('raw_message')
-        self.text = re.sub(r'\[CQ:(.*?)\]', '', js['raw_message'])
-        self.isGroup = js['message_type'] == 'group'
-        self.at_me = f'[CQ:at,qq={js["self_id"]}]' in js['message']
+        # 群聊参数
+        self.group_id = js.get('group_id')
+        # 频道参数
+        self.channel_id = js.get('channel_id')
+        self.guild_id = js.get('guild_id')
+        self.self_tiny_id = js.get('self_tiny_id')
+        # 特殊参数
+        self.real_content = ''
+        self.text = re.sub(r'\[CQ:(.*?)\]', '', self.message)
+        self.at_me = f'[CQ:at,qq={self.self_id}]' in self.message
 
     def __str__(self):
-        return self.message
+        return self.message.strip()
+
+    @property
+    def content(self):
+        return self.real_content.strip()
 
     def split(self, split_text=' '):
-        msg = self.message.strip().split(split_text)
-        return msg[0], msg[1:]
-
-    def limit(person=True, group=True, at=False, both=False):
-        def check_person(user_id):
-            if person is True:
-                return True
-            elif isinstance(person, int):
-                if person > 0 and user_id == person:
-                    return True
-                elif person < 0 and not user_id == -person:
-                    return True
-            elif isinstance(person, (list, tuple)):
-                if len(person) == 0:
-                    return False
-                if person[0] > 0:
-                    for i in person:
-                        if user_id == i:
-                            return True
-                elif person[0] < 0:
-                    for i in person:
-                        if user_id == -i:
-                            return False
-                    return True
-
-        def lim(func):
-            @wraps(func)
-            def wrapped_function(self):
-                if self.isGroup:
-                    if at and not self.at_me:
-                        return void_func()
-                    if both and not check_person(self.user_id):
-                        return void_func()
-                    if group is True:
-                        return func(self)
-                    elif isinstance(group, int):
-                        if group > 0 and self.group_id == group:
-                            return func(self)
-                        elif group < 0 and not self.group_id == -group:
-                            return func(self)
-                        else:
-                            return void_func()
-                    elif isinstance(group, (list, tuple)):
-                        if len(group) == 0:
-                            return void_func()
-                        if group[0] > 0:
-                            for i in group:
-                                if self.group_id == i:
-                                    return func(self)
-                        elif group[0] < 0:
-                            for i in group:
-                                if self.group_id == -i:
-                                    return void_func()
-                            return func(self)
-                        else:
-                            return void_func()
-                else:
-                    return func(self) if check_person(self.user_id) else void_func()
-            return wrapped_function
-        return lim
-
-    def on_command(need=None):
-        def check(func):
-            @wraps(func)
-            def wrapped_function(self):
-                command, args = self.split()
-                if need == command or not need:
-                    self.args = args
-                    return func(self)
-                else:
-                    return void_func()
-            return wrapped_function
-        return check
+        return self.message.strip().split(split_text)
 
 
-class PrivateMessage(Message):
-    def __init__(self, js):
-        super().__init__(js)
-
-
-class GroupMessage(Message):
-    def __init__(self, js):
-        super().__init__(js)
-        if 'group_id' in js:
-            self.group_id = js['group_id']
-
-
-def get_event_from_msg(msg: str):
+def get_event_from_msg(msg: bytes) -> Event:
     js = loads(msg)
-    pt = js.get('post_type')
-    if pt == 'message':
-        if js['message_type'] == 'private':
-            return PrivateMessage(js)
-        elif js['message_type'] == 'group':
-            return GroupMessage(js)
-    elif pt == 'meta_event':
-        return Mate(js)
+    match js.get('post_type'):
+        case 'message':
+            return Message(js)
+        case 'meta_event':
+            return Mate(js)
