dada101c94ddc7a845260f0bedaae54642ca7538
Drelf2018 <drelf2018@outlook.com>
2022-09-14 00:19:34 +0800

    feat: 新增评论爬虫

--- plugins/weibo.py
+++ plugins/weibo.py
@@ -6,9 +6,13 @@ from nana7mi.adapters.cqbot import cqBot
 from yaml import Loader, dump, load
 
 from plugins.weibo_detector.d2p import create_new_img
-from plugins.weibo_detector.weibo import get_data, get_post, get_userInfo, get_comments
+from plugins.weibo_detector.weiboSpider import get_data, get_detail, get_post, get_userInfo, get_comments
 
+# 获取 cqBot 适配器
 bot = get_driver()
+cb: cqBot = bot.cqbot
+
+# 读取已保存配置文件（博文、评论等）
 FILEDIR = os.path.dirname(__file__)
 with open(f'{FILEDIR}/weibo_detector/content.yml', 'r', encoding='utf-8') as fp:
     Content = load(fp, Loader=Loader)
@@ -32,66 +36,67 @@ headers = {
 
 async def weibo(uid):
     log.info(f'正在更新用户 {uid} 微博', 'Weibo')
-    session = httpx.AsyncClient(headers=headers)
-    try:
-        data = await get_data(session, uid)
-        userInfo = dict()
-    except Exception as e:
-        log.error(f'更新用户 {uid} 微博失败 {e}', 'Weibo')
-        return
-
-    for i in range(1, 4):  # 爬取前 3 条
+    async with httpx.AsyncClient(headers=headers) as session:
         try:
-            post = await get_post(session, data, i)
+            data = await get_data(session, uid)
+            userInfo = dict()
         except Exception as e:
-            log.error(f'爬取用户 {uid} 第 {i} 条微博失败 {e}', 'Weibo')
-            continue
+            log.error(f'更新用户 {uid} 微博失败 {e}', 'Weibo')
+            return
 
-        # 检测微博是否存在或者是否发生变化
-        if Content.get(post['mid'], {}).get('content') != (rtext := post['repo'] if post['repo'] else post['text']):
+        for i in range(1, 4):  # 爬取前 3 条
+            try:
+                post = await get_post(session, data, i)
+            except Exception as e:
+                log.error(f'爬取用户 {uid} 第 {i} 条微博失败 {e}', 'Weibo')
+                continue
 
-            # 生成图片并发送动态
-            log.info(f'用户 {uid} 微博 {post["mid"]} 搬运中', 'Weibo')
+            # 检测微博是否存在或者是否发生变化
+            rtext = post['repo'][0] if post.get('repo') else post.get('text', [[]])[0]
+            if Content.get(post['mid'], {}).get('content') != rtext:
 
-            # 获取博主信息
-            if not userInfo:
-                try:
-                    userInfo = await get_userInfo(session, uid)
-                except Exception as e:
-                    log.error(f'爬取用户 {uid} 信息失败 {e}', 'Weibo')
+                # 生成图片并发送动态
+                log.info(f'用户 {uid} 微博 {post["mid"]} 搬运中', 'Weibo')
+                
+                # 更新内容
+                post = await get_detail(session, post)
 
-            # 获取 cqBot 适配器
-            cb: cqBot = bot.cqbot
+                # 获取博主信息
+                if not userInfo:
+                    try:
+                        userInfo = await get_userInfo(session, uid)
+                    except Exception as e:
+                        log.error(f'爬取用户 {uid} 信息失败 {e}', 'Weibo')
 
-            # 文字版
-            msg = '{name}\n粉丝 {follower} | 关注 {follow}\n发送了微博:\n'.format_map(userInfo)
-            if post['repo']:
-                msg += '\n{repo}\n----------'.format_map(post)
-            msg += '\n{text}\n\n{time}'.format_map(post)
+                # 文字版
+                msg = '{name}\n粉丝 {follower} | 关注 {follow}\n发送了微博:\n'.format_map(userInfo)
+                if post.get('repo'):
+                    msg += '\n{repo}\n----------'.format_map(post)
+                msg += '\n{text}\n\n{time}'.format_map(post)
 
-            await cb.send_guild_msg(76861801659641160, 9638022, msg)
+                await cb.send_guild_msg(76861801659641160, 9638022, msg)
+                
+                # 图片版
+                image = await create_new_img(post, userInfo)
+                image.save(cb.PATH + '/data/images/wb/'+post['mid']+'.png')
+
+                await cb.send_guild_msg(76861801659641160, 9638022, '[CQ:image,file=wb/{mid}.png]'.format_map(post))
+
+                # 发生变化保存到文件
+                Content[post['mid']] = {'content': rtext, 'comment': Content.get(post['mid'], {}).get('comment', list())}
+                save()
             
-            # 图片版
-            image = await create_new_img(post, userInfo, headers)
-            image.save(cb.PATH + '/data/images/wb/'+post['mid']+'.png')
-
-            await cb.send_guild_msg(76861801659641160, 9638022, '[CQ:image,file=wb/{mid}.png]'.format_map(post))
-
-            # 发生变化保存到文件
-            Content[post['mid']] = {'content': rtext, 'comment': Content.get(post['mid'], {}).get('comment', list())}
-            save()
-        
-        # 爬最新一条微博的评论 本来想用的 但是频繁了 爬不到数据了
-        # elif i == 1 and uid == 7198559139:
-        #     cmt = await get_comments(session, post["mid"])
-        #     old = set(Content[post['mid']].get('comment', set()))
-        #     new = cmt.difference(old)
-        #     if new:
-        #         log.info(f'用户 {uid} 微博 {post["mid"]} 评论 {new}', 'Weibo')
-        #         Content[post['mid']]['comment'] = list(old | cmt)
-        #         save()
-        #         for c in new:
-        #             await bot.cqbot.send_guild_msg(76861801659641160, 9638022, c)
+            # 爬最新一条微博的最新评论 本来想用的 但是频繁了 爬不到数据了
+            elif i==1 and uid == 7198559139:
+                cmt = await get_comments(session, post["mid"])
+                old = set(Content[post['mid']].get('comment', set()))
+                new = cmt.difference(old)
+                if new:
+                    log.info(f'用户 {uid} 微博 {post["mid"]} 评论 {new}', 'Weibo')
+                    Content[post['mid']]['comment'] = list(old | cmt)
+                    save()
+                    for c in new:
+                        await cb.send_guild_msg(76861801659641160, 9638022, c)
 
 # 七海Nana7mi 微博监控
 bot.sched.add_job(weibo, 'interval', seconds=10, next_run_time=bot.run_time(10), args=[7198559139])
