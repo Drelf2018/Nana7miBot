548cfe229160903faa54cbfcad3bdbc83ec2e170
Drelf2018 <drelf2018@outlook.com>
2022-10-08 23:17:56 +0800

    Update acrnm.py

--- plugins/acrnm.py
+++ plugins/acrnm.py
@@ -1,6 +1,7 @@
-import httpx
 import asyncio
-from lxml import etree
+import json
+
+import httpx
 from nana7mi import get_driver, log
 from nana7mi.adapters import cqBot
 from nana7mi.adapters.event import Message
@@ -11,53 +12,36 @@ bot = get_driver()
 cb: cqBot = bot.cqbot
 
 async def get_list(init=False):
-    resp = httpx.get(BASEURL, timeout=250.0)
-    data = etree.HTML(resp.text)
-    new_products = dict()
-    for tr in data.xpath('.//tbody/tr'):
-        msgs = ""
-        for td in tr.xpath('./td'):
-            match tag := td.xpath("./@class")[0][39:-5]:
-                case "title":
-                    name = td.xpath('.//span/text()')[0]
-                    msgs += name + '\n'
-                    if name not in Products:
-                        Products[name] = dict()
-                        Products[name]["variant"] = dict()
-                        Products[name]["price"] = dict()
-                case "variant":
-                    for span in td.xpath('./div/span'):
-                        color = '/'.join(span.xpath("./div/span/text()"))
-                        size = '/'.join(span.xpath("./span/text()"))       
-                        Products[name][tag][color] = size
-                case "price":
-                    if val := td.xpath('.//span/text()'):
-                        if val[0] not in Products[name][tag]:
-                            msgs += f'{tag}: {val[0]}\n'
-                            Products[name][tag][val[0]] = None
-                        if Products[name][tag][val[0]] != Products[name]["variant"]:
-                            Products[name][tag][val[0]] = Products[name]["variant"]
-                            for c, s in Products[name]["variant"].items():
-                                msgs += f'{c}: {s}\n'
-                        Products[name]["variant"] = dict()
-                case _:
-                    if val := td.xpath('.//span/text()'):
-                        if Products[name].get(tag) != val[0]:
-                            msgs += f'{tag}: {val[0]}\n'
-                        Products[name][tag] = val[0]
-        if msgs != name + '\n': new_products[name] = msgs
-    
-    if init: return
-
-    if new_products: imgs = etree.HTML(httpx.get("https://acrnm.com/").text)
-    for name, msgs in new_products.items():
-        try:
-            img = "https://acrnm.com/" + imgs.xpath(f'.//span[text()="{name}"]/../img/@src')[0]
-            msgs = msgs.strip() + f'[CQ:image,file={img}]'
-        except:
-            ...
-        log.info(msgs, 'acrnm')
-        await cb.send_private_msg(2086378741, msgs)
+    global Products
+    new = httpx.get('http://aliyun.nana7mi.link/get_list', timeout=20.0).text
+    new = json.loads(new)
+    for name, val in new.items():
+        msgs = name + '\n'
+        if name not in Products:
+            for p, li in val['price'].items():
+                msgs += p + '\n'
+                for color, size in li.items():
+                    msgs += f'{color}: {size}\n'
+        else:
+            price = Products[name]["price"]
+            for p, li in val['price'].items():
+                if p in price:
+                    flag = True
+                    for color, size in li.items():
+                        if price[p][color] != size:
+                            if flag:
+                                msgs += p + '\n'
+                                flag = False
+                            msgs += f'{color}: {size}\n'
+                else:
+                    msgs += p + '\n'
+                    for color, size in li.items():
+                        msgs += f'{color}: {size}\n'
+        if not init and msgs != name + '\n':
+            msgs = msgs.strip() + f'[CQ:image,file={val["img"]}]'
+            log.info(msgs, 'acrnm')
+            await cb.send_private_msg(2086378741, msgs)
+    Products = new
 
 @bot.sched.scheduled_job('interval', seconds=300, next_run_time=bot.run_time(30))
 async def main():
